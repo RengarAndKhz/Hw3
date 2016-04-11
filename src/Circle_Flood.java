b6d1f8bf32522e2d43055c2e027ec7ae2e8217a6
tianyang <wangtyang@gmail.com>
2016-04-11 15:08:30 -0400

    test again

--- src/Circle_Flood.java
+++ src/Circle_Flood.java
@@ -1,11 +1,15 @@
 /**
  * Created by Tianyang on 4/9/16.
  */
+import ij.IJ;
 import ij.ImagePlus;
 import ij.plugin.filter.PlugInFilter;
+import ij.process.ImageConverter;
 import ij.process.ImageProcessor;
 
 public class Circle_Flood implements PlugInFilter{
+    final int pMax = 40;
+    final int pMin = 10;
     @Override
     public int setup(String s, ImagePlus imagePlus) {
         return DOES_ALL;
@@ -13,7 +17,17 @@ public class Circle_Flood implements PlugInFilter{
 
     @Override
     public void run(ImageProcessor imageProcessor) {
-
-
+        circularHough();
     }
+
+    public void circularHough(){
+        ImagePlus imp = IJ.getImage();
+        ImageConverter ic = new ImageConverter(imp);
+        //ImageConverter ic = new ImageConverter(new ImagePlus("", imp));
+        ic.convertToGray8();
+        ImageProcessor imageProcessor = imp.getProcessor().duplicate();
+        imageProcessor.threshold(90);
+        new ImagePlus("", imageProcessor).show();
+    }
+
 }
